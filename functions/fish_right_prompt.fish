990cf6a18be9761f10a10be404135e6c8aa9fe8a
M Nasimul Haque <nasim.haque@gmail.com>
2014-02-23 18:34:11 +0000

    right prompt in segments

--- functions/fish_right_prompt.fish
+++ functions/fish_right_prompt.fish
@@ -1,5 +1,13 @@
+set left_black_arrow_glyph  \u2B82
+set bk_color 3a2a03
+set fore_color c99
+
 function fish_right_prompt
-  set_color $fish_color_autosuggestion[1]
-  date "+%m-%d %H:%M:%S"
+  set_color $bk_color
+  echo -n "$left_black_arrow_glyph"
+  set_color -b $bk_color
+  set_color $fore_color --bold
+  set current_date (date "+%m-%d %H:%M:%S")
+  echo -n " $current_date "
   set_color normal
 end
