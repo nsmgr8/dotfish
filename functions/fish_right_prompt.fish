7d1f942afbde2cae55b856ff674a8763b5b0e3eb
M Nasimul Haque <nasim.haque@gmail.com>
2014-02-24 00:00:29 +0000

    show execution time

--- functions/fish_right_prompt.fish
+++ functions/fish_right_prompt.fish
@@ -1,13 +1,23 @@
 set left_black_arrow_glyph  \u2B82
-set bk_color 3a2a03
-set fore_color c99
+set left_arrow_glyph        \uE0B3
 
 function fish_right_prompt
-  set_color $bk_color
+  cmd_time_taken
+  set_color 323
   echo -n "$left_black_arrow_glyph"
-  set_color -b $bk_color
-  set_color $fore_color --bold
+  set_color -b 323
+  set_color 9c9
   set current_date (date "+%m-%d %H:%M:%S")
   echo -n " $current_date "
   set_color normal
+end
+
+function cmd_time_taken
+  if [ -n (echo $CMD_DURATION | tr " " d) ]
+    set_color 600
+    echo -n "$left_black_arrow_glyph"
+    set_color -b 600
+    set_color c99 --bold
+    echo -n " $CMD_DURATION "
+  end
 end
