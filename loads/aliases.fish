9038c36467d84d8fb65494677579dbf3b3cd3e0c
M Nasimul Haque <nasim.haque@gmail.com>
2016-11-25 09:54:19 +0000

    prefer py3 when available

--- loads/aliases.fish
+++ loads/aliases.fish
@@ -8,4 +8,4 @@ alias pc "pushd (pwd)"
 alias pu "pushd"
 alias po "popd"
 
-alias serve_http 'python -m SimpleHTTPServer'
+alias serve_http 'python3 --version; and python3 -m http.server; or python -m SimpleHTTPServer'
