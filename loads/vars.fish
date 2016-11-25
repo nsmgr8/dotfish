87314b67e3852ee4c178c12744a8833d8c867c62
M Nasimul Haque <nasim.haque@gmail.com>
2016-11-25 09:54:51 +0000

    check existence of folder before adding to PATH

--- loads/vars.fish
+++ loads/vars.fish
@@ -1,9 +1,23 @@
-set PATH $HOME/bin \
+function __set_extra_path
+    if [ -d $1 ]
+        set PATH $PATH $1
+    end
+end
+
+set __extra_paths \
+    $HOME/bin \
     /usr/local/share/npm/bin \
-    /usr/local/bin /usr/local/sbin \
-    /usr/bin /usr/sbin \
+    /usr/local/bin \
+    /usr/local/sbin \
+    /usr/bin \
+    /usr/sbin \
     /bin /sbin \
-    /usr/texbin /usr/games
+    /usr/texbin \
+    /usr/games
+
+for path in $__extra_paths
+    __set_extra_path $path
+end
 
 set TERM xterm-256color
 set EDITOR vim
