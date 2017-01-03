8ad4667b252876eccb324998789634ebc3d168ea
M Nasimul Haque <nasim.haque@gmail.com>
2017-01-03 11:35:18 +0000

    tell fortune when exists

--- functions/fish_greeting.fish
+++ functions/fish_greeting.fish
@@ -3,6 +3,16 @@ function fish_greeting
   uname -npsr
   set_color $fish_color_quote
   uptime
-  fortune -s | cowsay
+  if which fortune >/dev/null ^&1
+      set quote (fortune -s)
+      if which cowsay >/dev/null ^&1
+        set cow (cowsay -l | tail -n +2 | tr ' ' '\n' | sort -R | head -1)
+        echo $quote | cowsay -f $cow
+      else
+        echo
+        echo $quote
+        echo
+      end
+  end
   set_color normal
 end
